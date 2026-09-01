4bdae37c90c85584da6b3dbf45f8cf0bc9a1233e
Ahmad Muhammad Gulu <ahmadmuhammadgulu002@gmail.com>
2026-09-01 19:06:46 +0100

    add comment to app

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@
       import{useEffect} from "react";
       import "./App.css";
      
-
+      // Calcutor app
       export default function App(){
         
         const [input, setInput] = useState("");
